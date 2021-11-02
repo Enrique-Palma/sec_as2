ca04ea579670f1785fc35e128975945162601ca3
Enrique Palma <72711747+Enrique-Palma@users.noreply.github.com>
2021-11-02 09:56:54 -0400

    Update Source.c
    test

--- sec_as2/Source.c
+++ sec_as2/Source.c
@@ -146,7 +146,7 @@ int main(int argc, char** argv)
 			// Pad with a necessary X
 			if (checkStringLength(input) % 2)
 				my_strcat(input, "X");
-			//strcat(input, "X");
+			
 			print80(input);
 			printf("\n");
 			res16bit = calc16bit(input);
@@ -157,7 +157,7 @@ int main(int argc, char** argv)
 			// Pad with necessary X's
 			while (checkStringLength(input) % 4)
 				my_strcat(input, "X");
-			//strcat(input, "X");
+			
 			print80(input);
 			printf("\n");
 			res32bit = calc32bit(input);
